e0b206aea073e82725d15127b5729d3b54c7e93f
David Gili <d.gili.lopez@gmail.com>
2026-02-01 22:20:07 -0300

    total de la cuenta funciona

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,10 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState("0");
+
+  const calcularTotal = () => {
+    let total = 0;
+    const carrito = [...cart];
+    carrito.forEach((element) => (total += element.price * element.count));
+    setTotal(total);
+  };
 
   const agregarPizza = (pizza) => {
     const nuevaPizza = [...cart];
@@ -34,6 +41,10 @@ const CartProvider = ({ children }) => {
       setCart(restarPizza);
     }
   };
+
+  useEffect(() => {
+    calcularTotal();
+  }, [cart]);
 
   return (
     <CartContext.Provider value={{ cart, total, agregarPizza, quitarPizza }}>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,45 +1,9 @@
 import Button from "react-bootstrap/esm/Button";
-import { pizzaCart } from "../assets/pizzas";
 import { useContext, useState } from "react";
 import { CartContext } from "../context/CartContext";
 
 const Cart = () => {
-  // const [cart, setCart] = useState(pizzaCart);
   const { cart, total, agregarPizza, quitarPizza } = useContext(CartContext);
-
-  // const agregarPizza = (pizza) => {
-  //   const nuevaPizza = [...cart];
-  //   const indice = nuevaPizza.findIndex(
-  //     (element) => element.name === pizza.name,
-  //   );
-  //   nuevaPizza[indice].count += 1;
-  //   setCart(nuevaPizza);
-  // };
-
-  // const quitarPizza = (pizza) => {
-  //   const restarPizza = [...cart];
-  //   const indice = restarPizza.findIndex(
-  //     (element) => element.name === pizza.name,
-  //   );
-  //   if (restarPizza[indice].count === 1) {
-  //     const quitarPizza = restarPizza.filter(
-  //       (element) => element.name !== pizza.name,
-  //     );
-  //     setCart(quitarPizza);
-  //   } else {
-  //     restarPizza[indice].count -= 1;
-  //     setCart(restarPizza);
-  //   }
-  // };
-
-  // const calcularTotal = () => {
-  //   let total = 0;
-  //   const carrito = [...cart];
-  //   carrito.forEach((element) => (total += element.price * element.count));
-  //   return total;
-  // };
-
-  // const total = calcularTotal();
 
   return (
     <div className="carro">
@@ -48,7 +12,7 @@ const Cart = () => {
           <p>Detalles del pedido:</p>
         </div>
         <div className="Detalle">
-          {cart?.map((pizza) => {
+          {cart.map((pizza) => {
             return (
               <div className="pizzaCarrito" key={pizza.id}>
                 <div>
